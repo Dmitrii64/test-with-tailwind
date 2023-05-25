31074c29984962fc022d9771f0d2f15aa7a434e5
Dmitrii64 <60173944+Dmitrii64@users.noreply.github.com>
2023-05-25 20:11:17 +0400

    Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {transaction} from "./data/transaction";
+import {types} from "./data/types";
+import {useEffect, useState} from "react";
+import {useLocation, Link} from "react-router-dom"
 
 function App() {
+  const transactionTypes = types;
+  const location = useLocation();
+  const activeTabIndex = location.search ? location.search.slice(-1) : 0;
+  const [activeType, setActiveType] = useState(transactionTypes[activeTabIndex].title);
+  const [transactionsList, setTransactionsList] = useState(transaction.data);
+
+  function addRandomAmount() {
+    let newList = [];
+    transactionsList.forEach(transaction => {
+      transaction.amount = (Math.random() * 4000).toFixed(2);
+      newList.push(transaction)
+    });
+    setTransactionsList(newList)
+  }
+
+  useEffect(() => {
+    addRandomAmount()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container mx-auto my-10">
+        <div className="mx-auto max-w-fit">
+          <ul className="flex mx-auto max-w-fit">
+            {transactionTypes.map(type => (
+                <Link key={type.id} to={`/navigator?tab=${type.id - 1}`}>
+                  <li className={`px-5 py-2 border-2 border-black rounded-t-lg ${activeType === type.title ? 'bg-gray-100' : 'cursor-pointer'}`}
+                      onClick={() => setActiveType(type.title)}>
+                    {type.title}
+                  </li>
+                </Link>
+            ))}
+          </ul>
+          <ul>
+            <li className="flex justify-between px-10 py-2 border-b font-bold">
+              <p>Name</p>
+              <p>Amount</p>
+            </li>
+            {transactionsList.map(transaction => transaction.type === activeType && (
+                <li key={transaction._id} className="flex justify-between px-10 py-2 border-b">
+                  <p>{transaction.name.first} {transaction.name.last}</p>
+                  <p>{transaction.amount}</p>
+                </li>
+            ))}
+          </ul>
+        </div>
+      </div>
   );
 }
 
---
+++ src/data/types.jsx
@@ -0,0 +1,18 @@
+export const types = [
+  {
+    id: 1,
+    title: 'income'
+  },
+  {
+    id: 2,
+    title: 'outcome'
+  },
+  {
+    id: 3,
+    title: 'loan'
+  },
+  {
+    id: 4,
+    title: 'investment'
+  },
+]
